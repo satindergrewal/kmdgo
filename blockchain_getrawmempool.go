3faf7614b4bf9580dd0ecae01bf42efc9d972dc1
Satinder Grewal <grewal@lavabit.com>
2019-01-15 21:49:09 +1300

    getrawmempool methods updated

--- blockchain_getrawmempool.go
+++ blockchain_getrawmempool.go
@@ -7,7 +7,8 @@ import (
 	"strconv"
 )
 
-type GetRawMempool struct {
+
+type GetRawMempoolTrue struct {
 	Result map[string]RawMempoolTrue	`json:"result"`
 	Error	Error	`json:"error"`
 	ID		string	`json:"id"`
@@ -23,14 +24,49 @@ type RawMempoolTrue struct {
 	Depends          []interface{} `json:"depends"`
 }
 
-func (appName AppType) GetRawMempool(b bool) (GetRawMempool, error) {
+type GetRawMempoolFalse struct {
+	Result []string	`json:"result"`
+	Error	Error	`json:"error"`
+	ID		string	`json:"id"`
+}
+
+func (appName AppType) GetRawMempoolTrue(b bool) (GetRawMempoolTrue, error) {
 	query := APIQuery {
 		Method:	`getrawmempool`,
 		Params:	`[`+strconv.FormatBool(b)+`]`,
 	}
 	//fmt.Println(query)
 
-	var getrawmempool GetRawMempool
+	var getrawmempool GetRawMempoolTrue
+
+	getrawmempoolJson := appName.APICall(query)
+	//fmt.Println(getrawmempoolJson)
+
+	var result APIResult
+
+	json.Unmarshal([]byte(getrawmempoolJson), &result)
+
+	if result.Result == nil {
+		answer_error, err := json.Marshal(result.Error)
+		if err != nil {
+		}
+		json.Unmarshal([]byte(getrawmempoolJson), &getrawmempool)
+		return getrawmempool, errors.New(string(answer_error))
+	}
+
+	json.Unmarshal([]byte(getrawmempoolJson), &getrawmempool)
+	return getrawmempool, nil
+}
+
+
+func (appName AppType) GetRawMempoolFalse(b bool) (GetRawMempoolFalse, error) {
+	query := APIQuery {
+		Method:	`getrawmempool`,
+		Params:	`[`+strconv.FormatBool(b)+`]`,
+	}
+	//fmt.Println(query)
+
+	var getrawmempool GetRawMempoolFalse
 
 	getrawmempoolJson := appName.APICall(query)
 	//fmt.Println(getrawmempoolJson)
--- examples/getrawmempool.go
+++ examples/getrawmempool.go
@@ -10,8 +10,8 @@ func main() {
 	var appName kmdgo.AppType
 	appName = `komodo`
 
-	var mpool kmdgo.GetRawMempool
-	mpool, err := appName.GetRawMempool(true)
+	var mpool kmdgo.GetRawMempoolTrue
+	mpool, err := appName.GetRawMempoolTrue(true)
 	if err != nil {
 		fmt.Printf("Code: %v\n", mpool.Error.Code)
 		fmt.Printf("Message: %v\n\n", mpool.Error.Message)
@@ -27,5 +27,27 @@ func main() {
 		fmt.Println(i)
 		fmt.Println(v)
 		fmt.Println(v.Size)
+	}
+
+	fmt.Printf("\n\n\n\n")
+
+	var rmem kmdgo.GetRawMempoolFalse
+	rmem, err = appName.GetRawMempoolFalse(false)
+	if err != nil {
+		fmt.Printf("Code: %v\n", rmem.Error.Code)
+		fmt.Printf("Message: %v\n\n", rmem.Error.Message)
+		log.Fatalln("Err happened", err)
+	}
+
+	fmt.Println("rmem value", rmem)
+	fmt.Println()
+	fmt.Println(rmem.Result)
+
+	fmt.Println("-----")
+	fmt.Println(len(rmem.Result))
+	for i, v := range rmem.Result {
+		fmt.Println(i)
+		fmt.Println(v)
+		//fmt.Println(v.Size)
 	}
 }
