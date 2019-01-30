a6162c757394ffe0a637a75d10d7075e0cf720a2
Satinder Grewal <grewal@lavabit.com>
2019-01-30 19:53:10 +1300

    doc.go test file

---
+++ doc.go
@@ -0,0 +1,14 @@
+// Copyright © 2018-2019 Satinderjit Singh.
+// See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at
+// the top-level directory of this distribution for the individual copyright
+// holder information and the developer policies on copyright and licensing.
+// Unless otherwise agreed in a custom licensing agreement, no part of the
+// kmdgo software, including this file may be copied, modified, propagated.
+// or distributed except according to the terms contained in the LICENSE file
+// Removal or modification of this copyright notice is prohibited.
+
+/*
+This is example line in doc.go
+*/
+
+package kmdgo
