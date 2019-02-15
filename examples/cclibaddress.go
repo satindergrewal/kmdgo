a870f07518194851b35ddae232c5a27a31479a7c
Satinder Grewal <grewal@lavabit.com>
2019-02-15 17:53:32 +1300

    cclibaddress example update

--- examples/cclibaddress.go
+++ examples/cclibaddress.go
@@ -23,24 +23,35 @@ import (
 func main() {
 	appName := kmdgo.NewAppType(`ROGUE`)
 
-	var rgng kmdgo.CCLibAddress
+	var ccladdr kmdgo.CCLibAddress
 
 	args := make(kmdgo.APIParams, 2)
 	args[0] = `17`
 	args[1] = `03778d6e5d4d20482ef8c72f5ee1f458ac0ebf1333a03e57187c436568eaf7ac31`
 	fmt.Println(args)
 
-	rgng, err := appName.CCLibAddress(args)
+	ccladdr, err := appName.CCLibAddress(args)
 	if err != nil {
-		fmt.Printf("Code: %v\n", rgng.Error.Code)
-		fmt.Printf("Message: %v\n\n", rgng.Error.Message)
+		fmt.Printf("Code: %v\n", ccladdr.Error.Code)
+		fmt.Printf("Message: %v\n\n", ccladdr.Error.Message)
 		log.Fatalln("Err happened", err)
 	}
 
-	fmt.Println("rgng value", rgng)
+	fmt.Println("ccladdr value", ccladdr)
 	fmt.Println("-------")
-	fmt.Println(rgng.Result)
+	fmt.Println(ccladdr.Result)
 
 	fmt.Println("\n")
 
+	fmt.Println("Result: ", ccladdr.Result.Result)
+	fmt.Println("CClibCCAddress: ", ccladdr.Result.CClibCCAddress)
+	fmt.Println("CCbalance: ", ccladdr.Result.CCbalance)
+	fmt.Println("CClibNormalAddress: ", ccladdr.Result.CClibNormalAddress)
+	fmt.Println("CClibCCTokensAddress: ", ccladdr.Result.CClibCCTokensAddress)
+	fmt.Println("MyAddress: ", ccladdr.Result.MyAddress)
+	fmt.Println("MyCCAddressCClib: ", ccladdr.Result.MyCCAddressCClib)
+	fmt.Println("MyCCaddress: ", ccladdr.Result.MyCCaddress)
+	fmt.Println("MyCCbalance: ", ccladdr.Result.MyCCbalance)
+	fmt.Println("Myaddress: ", ccladdr.Result.Myaddress)
+	fmt.Println("Mybalance: ", ccladdr.Result.Mybalance)
 }
