3048414462b3f04129c1a7e20319a8567cd2293d
Satinder Grewal <grewal@lavabit.com>
2019-02-15 14:47:22 +1300

    CC Rogue method newgame added, cleaned files and folders a tiny bit

---
+++ cc_rogue.go
@@ -0,0 +1,75 @@
+// Copyright © 2018-2019 Satinderjit Singh.
+//
+// See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at
+// the top-level directory of this distribution for the individual copyright
+// holder information and the developer policies on copyright and licensing.
+//
+// Unless otherwise agreed in a custom licensing agreement, no part of the
+// kmdgo software, including this file may be copied, modified, propagated.
+// or distributed except according to the terms contained in the LICENSE file
+//
+// Removal or modification of this copyright notice is prohibited.
+
+
+
+package kmdgo
+
+import (
+	//"fmt"
+	"encoding/json"
+	"errors"
+)
+
+type RGNewGame struct {
+	Result struct {
+		Name       string  `json:"name"`
+		Method     string  `json:"method"`
+		Maxplayers int     `json:"maxplayers"`
+		Buyin      float64 `json:"buyin"`
+		Type       string  `json:"type"`
+		Hex        string  `json:"hex"`
+		Txid       string  `json:"txid"`
+		Result     string  `json:"result"`
+	} `json:"result"`
+	Error	Error	`json:"error"`
+	ID		string	`json:"id"`
+}
+
+func (appName AppType) RGNewGame(params APIParams) (RGNewGame, error) {
+	var params_json string
+
+	if params[0] == "" || params[0] == nil {
+		params_json = `[]`
+		//fmt.Println(params_json)
+	} else {
+		params_bytes, _ := json.Marshal(params)
+		params_json = string(params_bytes)
+		fmt.Println(params_json)
+	}
+
+	query := APIQuery {
+		Method:	`cclib`,
+		Params:	params_json,
+	}
+	//fmt.Println(query)
+
+	var faucetaddress RGNewGame
+
+	faucetaddressJson := appName.APICall(query)
+	//fmt.Println(faucetaddressJson)
+
+	var result APIResult
+
+	json.Unmarshal([]byte(faucetaddressJson), &result)
+
+	if result.Error != nil {
+		answer_error, err := json.Marshal(result.Error)
+		if err != nil {
+		}
+		json.Unmarshal([]byte(faucetaddressJson), &faucetaddress)
+		return faucetaddress, errors.New(string(answer_error))
+	}
+
+	json.Unmarshal([]byte(faucetaddressJson), &faucetaddress)
+	return faucetaddress, nil
+}
---
+++ examples/ccrogue/newgame.go
@@ -0,0 +1,55 @@
+// Copyright © 2018-2019 Satinderjit Singh.
+//
+// See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at
+// the top-level directory of this distribution for the individual copyright
+// holder information and the developer policies on copyright and licensing.
+//
+// Unless otherwise agreed in a custom licensing agreement, no part of the
+// kmdgo software, including this file may be copied, modified, propagated.
+// or distributed except according to the terms contained in the LICENSE file
+//
+// Removal or modification of this copyright notice is prohibited.
+
+
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"github.com/satindergrewal/kmdgo"
+)
+
+func main() {
+	appName := kmdgo.NewAppType(`ROGUE`)
+
+	var rgng kmdgo.RGNewGame
+
+	args := make(kmdgo.APIParams, 1)
+	args[0] = `newgame`
+	args[0] = `17`
+	args[0] = `[1]`
+	fmt.Println(args)
+
+	rgng, err := appName.RGNewGame(args)
+	if err != nil {
+		fmt.Printf("Code: %v\n", rgng.Error.Code)
+		fmt.Printf("Message: %v\n\n", rgng.Error.Message)
+		log.Fatalln("Err happened", err)
+	}
+
+	fmt.Println("rgng value", rgng)
+	fmt.Println("-------")
+	fmt.Println(rgng.Result)
+
+	fmt.Println("\n")
+
+	fmt.Println("Name: ", rgng.Result.Name)
+	fmt.Println("Method: ", rgng.Result.Method)
+	fmt.Println("Maxplayers: ", rgng.Result.Maxplayers)
+	fmt.Println("Buyin: ", rgng.Result.Buyin)
+	fmt.Println("Type: ", rgng.Result.Type)
+	fmt.Println("Hex: ", rgng.Result.Hex)
+	fmt.Println("Txid: ", rgng.Result.Txid)
+	fmt.Println("Result: ", rgng.Result.Result)
+}
