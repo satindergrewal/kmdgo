66a554d1dda1ff9c76b474fdf7542bff4618b3b8
Satinder Grewal <grewal@lavabit.com>
2019-02-16 00:31:31 +1300

    test

--- examples/ccrogue/playerinfo.go
+++ examples/ccrogue/playerinfo.go
@@ -50,7 +50,7 @@ func main() {
 	fmt.Println("Player->Data: ", rgpfo.Result.Player.Data)
 
 	for pi, pv := range rgpfo.Result.Player.Pack {
-		fmt.Printf("%v=> Player -> Pack: %s", pi, pv)
+		fmt.Printf("%v=> Player -> Pack: %s\n", pi, pv)
 	}
 	
 	fmt.Println("Player -> Packsize: ", rgpfo.Result.Player.Packsize)
