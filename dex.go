a2263d1fab399319c00f42cd263d53c152daf801
Satinder <grewal@lavabit.com>
2020-03-11 20:05:32 +1300

    test dex_anonsend code

---
+++ dex.go
@@ -0,0 +1,75 @@
+// Copyright © 2018-2019 Satinderjit Singh.
+//
+// See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at
+// the top-level directory of this distribution for the individual copyright
+// holder information and the developer policies on copyright and licensing.
+//
+// Unless otherwise agreed in a custom licensing agreement, no part of the
+// kmdgo software, including this file may be copied, modified, propagated.
+// or distributed except according to the terms contained in the LICENSE file
+//
+// Removal or modification of this copyright notice is prohibited.
+
+package kmdgo
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type DEXAnonsend struct {
+	Result struct {
+		Timestamp    int    `json:"timestamp"`
+		ID           int    `json:"id"`
+		Hash         string `json:"hash"`
+		TagA         string `json:"tagA"`
+		TagB         string `json:"tagB"`
+		Pubkey       string `json:"pubkey"`
+		Payload      string `json:"payload"`
+		Hex          int    `json:"hex"`
+		Decrypted    string `json:"decrypted"`
+		Decryptedhex int    `json:"decryptedhex"`
+		Senderpub    string `json:"senderpub"`
+		AmountA      string `json:"amountA"`
+		AmountB      string `json:"amountB"`
+		Priority     int    `json:"priority"`
+		Recvtime     int    `json:"recvtime"`
+		Cancelled    int    `json:"cancelled"`
+	} `json:"result"`
+	Error Error  `json:"error"`
+	ID    string `json:"id"`
+}
+
+func (appName AppType) DEXAnonsend(params APIParams) (DEXAnonsend, error) {
+
+	params_json, _ := json.Marshal(params)
+	fmt.Println(string(params_json))
+
+	query := APIQuery{
+		Method: `DEX_anonsend`,
+		Params: string(params_json),
+	}
+	fmt.Println(query)
+
+	// var dexAnonsend DEXAnonsend
+
+	// dexAnonsendJson := appName.APICall(query)
+	// if dexAnonsendJson == "EMPTY RPC INFO!" {
+	// 	return dexAnonsend, errors.New("EMPTY RPC INFO!")
+	// }
+
+	// var result APIResult
+
+	// json.Unmarshal([]byte(dexAnonsendJson), &result)
+
+	// if result.Result == nil {
+	// 	answer_error, err := json.Marshal(result.Error)
+	// 	if err != nil {
+	// 	}
+	// 	json.Unmarshal([]byte(dexAnonsendJson), &dexAnonsend)
+	// 	return dexAnonsend, errors.New(string(answer_error))
+	// }
+
+	// json.Unmarshal([]byte(dexAnonsendJson), &dexAnonsend)
+	// return dexAnonsend, nil
+}
