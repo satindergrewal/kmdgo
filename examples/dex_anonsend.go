611831c3c1b47626ccf436d0bf71a517f582501b
Satinder <grewal@lavabit.com>
2020-03-11 20:17:44 +1300

    update

--- examples/dex_anonsend.go
+++ examples/dex_anonsend.go
@@ -31,7 +31,7 @@ func main() {
 	args[2] = "0138d849d6bc81ff1c5389aae9a60ba3ee9cfd7858d93a3864679c25937e70951f"
 	fmt.Println(args)
 
-	anonsend, err := appName.ListUnspent(args)
+	anonsend, err := appName.DEXAnonsend(args)
 	if err != nil {
 		fmt.Printf("Code: %v\n", anonsend.Error.Code)
 		fmt.Printf("Message: %v\n\n", anonsend.Error.Message)
