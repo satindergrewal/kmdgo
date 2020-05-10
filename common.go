37c42f855af77806d3b7e8e411eb429ef65d7b54
Satinder <grewal@lavabit.com>
2020-05-11 03:36:06 +1200

    error fixed

--- common.go
+++ common.go
@@ -71,11 +71,11 @@ func NewAppType(app AppType) *AppType {
 //
 // As per this example, if for example using different SmartChain like "DEX", and the appName is set to example "DEX", just replace it word `komodo` with `DEX`.
 func (appName AppType) APICall(q *APIQuery) string {
-	// fmt.Println(appName)
-	if appName == "KMD" || appName == "Komodo" {
-		appName == AppType("komodo")
-	}
 	fmt.Println(appName)
+	if appName == "KMD" || appName == "Komodo" {
+		appName = AppType("komodo")
+		fmt.Println(appName)
+	}
 	var rpcurl, rpcuser, rpcpass, rpcport string
 
 	key := string(appName) + "_RPCURL"
