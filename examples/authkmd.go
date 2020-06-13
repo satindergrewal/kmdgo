58895193eb270d873d44703c188b13a999439b41
Satinder Grewal <grewal@lavabit.com>
2020-06-14 03:55:47 +1200

    display rpc info

--- examples/authkmd.go
+++ examples/authkmd.go
@@ -66,7 +66,9 @@ func basicAuth() string {
 	fmt.Println(appDir)
 
 	rpcuser, rpcpass, rpcport := kmdutil.AppRPCInfo(appName)
-	rpcuser, rpcpass, rpcport = "user2a66b68", "84fd254fbc5", "7771"
+	fmt.Println(rpcuser)
+	fmt.Println(rpcpass)
+	fmt.Println(rpcport)
 
 	client := &http.Client{}
 
