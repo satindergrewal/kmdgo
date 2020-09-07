2ee490de3d977047f2f95404e30a0f623d334d24
Satinder Grewal <grewal@lavabit.com>
2020-09-07 21:28:00 +1200

    saplinglib files structure update

--- saplinglib/src/Go/main.go
+++ saplinglib/src/Go/main.go
@@ -2,7 +2,7 @@ package main
 
 /*
 #cgo CFLAGS: -I../
-#cgo LDFLAGS: -L../ -lsaplinglib -lpthread -ldl -framework Security
+#cgo LDFLAGS: -L../../ -lsaplinglib -lpthread -ldl -framework Security
 #include "saplinglib.h"
 #include <stdlib.h>
 #include <string.h>
@@ -20,7 +20,7 @@ func main() {
 	// 1) nohd:				set it to false, if you don't want HD wallet
 	// 2) zcount:			the number of sapling addresses you want to generate
 	// 3) seed:				the user specified passphrase, which gives the same address everytime if given the same passphrase
-	// 4) is_iguana_seed:	set this to true if you want the output to always give a deterministic address based on user specified seed phrase
+	// 4) isIguanaSeed:		set this to true if you want the output to always give a deterministic address based on user specified seed phrase
 	nohd := C.bool(false)
 	zcount := C.uint(1)
 	seed := C.CString("user specified seed phrase")
